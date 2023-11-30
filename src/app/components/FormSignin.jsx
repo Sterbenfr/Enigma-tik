b27f59113adbd56513028779e9b3b2602ded22fd
Adel SIDI AHMED <adelsidiahmed2020@gmail.com>
2023-11-30 15:11:45 +0100

    version 0.2

---
+++ src/app/components/FormSignin.jsx
@@ -0,0 +1,44 @@
+const FormSignin = () => {
+  return (
+    <form className='card-body'>
+      <div className='form-control'>
+        <label className='label'>
+          <span className='label-text text-black'>Email</span>
+        </label>
+        <input
+          type='email'
+          placeholder='email'
+          className='input input-bordered  text-white bg-black'
+          required
+        />
+      </div>
+      <div className='form-control'>
+        <label className='label'>
+          <span className='label-text text-black '>Password</span>
+        </label>
+        <input
+          type='password'
+          placeholder='password'
+          className='input input-bordered text-white bg-black'
+          required
+        />
+      </div>
+      <div className='form-control'>
+        <label className='label'>
+          <span className='label-text text-black '>Password</span>
+        </label>
+        <input
+          type='password'
+          placeholder='password'
+          className='input input-bordered text-white bg-black'
+          required
+        />
+      </div>
+      <div className='form-control mt-6'>
+        <button className='btn text-white bg-black'>SIGNIN</button>
+      </div>
+    </form>
+  )
+}
+
+export default FormSignin
--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,27 +1,24 @@
 import Image from 'next/image'
-import logonavbar from '../../../public/image/logoENIGMA.png'
+import ButtonLogin from './ButtonLogin'
+import ButtonSignin from './ButtonSignin'
+import logonavbar from '../../../public/image/LogoBDELight.png'
 
 const NavBar = () => {
   return (
-    <nav className='top-0 w-full border-gray-200 bg-white'>
+    <nav className='top-0 w-full border-gray-200 bg-black'>
       <div className='container flex items-center justify-between mx-auto p-2'>
         <a href='' className='flex items-center'>
-          <Image src={logonavbar} alt="Description de l'image" width={100} />
+          <Image
+            src={logonavbar}
+            alt="Description de l'image"
+            width={35}
+            height={35}
+            className=' border-white rounded-sm shadow-2xl'
+          />
         </a>
-
         <div className='flex items-center'>
-          <button
-            type='button'
-            className='text-white bg-lime-800 hover:bg-lime-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-extralight rounded-lg text-sm px-1 py-1 text-center mr-1 md:mr-1 dark:bg-lime-700 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-          >
-            Se Connecter
-          </button>
-          <button
-            type='button'
-            className='text-white bg-lime-800 hover:bg-lime-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-extralight rounded-lg text-sm px-1 py-1 text-center mr-1 md:mr-1 dark:bg-lime-700 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-          >
-            Créer Un Compte
-          </button>
+          <ButtonLogin />
+          <ButtonSignin />
         </div>
       </div>
     </nav>
